be2840c3d501e1dc0df4f3b20b04f5786b82c617
agus-paez <162382545+agu-paez@users.noreply.github.com>
2026-06-17 03:06:24 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 
 
 const sequelize = new Sequelize('u591520106_bdsistema', 'u591520106_joel_benitez', 'Benitez_Tiziano_16/26', {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
     logging: false
 });
